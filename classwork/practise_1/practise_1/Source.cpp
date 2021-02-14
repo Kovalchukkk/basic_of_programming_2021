c3ff02ef7003e8473d5256a5fc88a3fd6d143ecd
Mykola Kovalchuk <kolyankov@gmail.com>
2021-02-14 13:48:52 +0200

    First app

---
+++ classwork/practise_1/practise_1/Source.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	cout << "Hello World!\n";
+	string abs = "I am Programmer!";
+	cout << abs;
+	return 0;
+
+	system("pause>0");
+}
