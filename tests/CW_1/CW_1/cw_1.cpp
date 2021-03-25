72abae9adefd34cbe4b064bbfa0c94d4b85c5074
Mykola Kovalchuk <kolyankov@gmail.com>
2021-03-25 20:33:39 +0200

    CW1

--- tests/CW_1/CW_1/cw_1.cpp
+++ tests/CW_1/CW_1/cw_1.cpp
@@ -109,26 +109,39 @@ Result: 568
 
 //3.
 
-//int Recursion(int number);
+int Rec(int number);
 int main()
 {
-	//int n;
-	//cout << "Enter a number: ";
-	//cin >> n;
-	//int a = Recursion(1);
-	//cout << a << endl;
+	int n;
+	cout << "Enter a number: ";
+	cin >> n;
+	int a = Rec(n);
+	cout << a << endl;
 	system("pause");
 	return 0;
 
 }
-/*
-int Recursion(int number)
+
+int Rec(int number)
 {
 	if (number == 0)
 		return 1;
 	if (number == 1)
 		return -1;
-	return	Recursion(4 * number * number - 1) + 1 - Recursion(number - 2);
-}*/
+	else
+		return	abs(4 * pow(Rec(number - 2), 2)) + 1 - Rec(number - 1);
+}
+
+//Examples
+/*
+Enter a number: 3
+-1
+Enter a number: 4
+146
+Enter a number: 5
+-141
+Enter a number: 6
+85406
+*/
 
 
