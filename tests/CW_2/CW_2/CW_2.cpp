c9b4cc0dc626651dfd1fc9638747eba5c466425e
Mykola Kovalchuk <kolyankov@gmail.com>
2021-04-08 18:08:44 +0300

    CW2

--- tests/CW_2/CW_2/CW_2.cpp
+++ tests/CW_2/CW_2/CW_2.cpp
@@ -96,55 +96,32 @@ Result array: 1  5  7  3  5  6  4  2  8  6
 */
 
 //2.
-/*
-int main_2()
-{
-	//Check size of matrix
-	int row, col;
-	do {
-		cout << "Enter a row and col: ";
-		cin >> row >> col;
-	} while (row != col);
 
-	int** matrix = new int* [row];
+int main()
+{
+	cout << "Enter row and col: ";
+
+	int n, m;
+	cin >> n >> m;
+
+	int** matrix = new int* [n];
+
 
 	//Read matrix
-	for (int i = 0; i < row; i++)
+	for (int i = 0; i < n; i++)
 	{
-		matrix[i] = new int[col];
-		for (int j = 0; j < col; j++)
+		matrix[i] = new int[m];
+		for (int j = 0; j < m; j++)
 		{
+			cout << "Enter matrix[" << i << "]" << "[" << j << "]" << "=" << "\t";
 			cin >> matrix[i][j];
 		}
 	}
-	system("pause");
-	return 0;
-
-	//Main program
-	double maxsum = 0;
-
-	for (int i = 0; i < row; i++)
-	{
-		int r = 0, c = i;
-
-		//Sum
-		double sum_1 = 0, sum_2 = 0;
-		while (c < row && r < row)
-		{
-			sum_1 += matrix[r][c];
-			sum_2 += matrix[c][r];
-			r++;
-			c++;
-		}
-		maxsum = max(sum_1, maxsum, sum_2);
-	}
-
-	cout << "Result: " << maxsum << endl;
 
 	//Print matrix
-	for (int i = 0; i < row; i++)
+	for (int i = 0; i < n; i++)
 	{
-		for (int j = 0; j < col; j++)
+		for (int j = 0; j < m; j++)
 		{
 			cout << matrix[i][j] << '\t';
 		}
@@ -152,38 +129,153 @@ int main_2()
 	}
 	cout << endl;
 
+	
+	//Sum of principal diagonal
+	double principalsum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		principalsum += matrix[i][i];
+	}
+
+	//Sum of elem above and below main diagonal
+	double abovesum = 0, belowsum = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < n; ++j)
+		{
+			if (j > i)
+			{
+				abovesum += matrix[i][j];
+			}
+			else
+			{
+				if (i > j)
+				{
+					belowsum += matrix[i][j];
+				}
+					
+			}
+				
+		}
+	}
+
+	//Main program
+	
+	if (principalsum > abovesum && principalsum > belowsum)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			cout << "Max diagonal: " << matrix[i][i] << ", ";
+		}
+		cout << endl;
+	}
+	else if (abovesum > principalsum && abovesum > belowsum)
+		cout << "Max sum diagonal: " << abovesum << endl;
+	else if (belowsum > principalsum && belowsum > abovesum)
+		cout << "Max sum diagonal: " << belowsum << endl;
+ 
 	//Delete matrix
-	for (int i = 0; i < row; i++)
+	for (int i = 0; i < n; i++)
 	{
 		delete[]matrix[i];
 	}
 	delete[]matrix;
-}*/
+}
 
 //3.
-
-
+/*
 void read_arr(int*& arr, int& n);
 void print_arr(int* arr, int n);
-bool check_anagram(int* arr1, int* arr2);
-int main()
+int check_anagram(int* arr1, int n1, int* arr2, int n2);
+void bubble_sort(int* arr, int n);
+int main_3()
 {
 	int* arr1;
 	int* arr2;
 	int n1;
 	int n2;
 	read_arr(arr1, n1);
-	print_arr(arr1, n1);
 	read_arr(arr2, n2);
+	print_arr(arr1, n1);
 	print_arr(arr2, n2);
-	bool check_anagram(arr1, arr2);
+	bubble_sort(arr1, n1);
+	bubble_sort(arr2, n2);
 
+	cout << "Res: " << check_anagram(arr1, n1, arr2, n2) << endl;
 	delete[] arr1;
 	delete[] arr2;
 	system("pause");
 	return 0;
 
-}
+}*/
+
+//Main function
+/*
+int check_anagram(int* arr1, int n1, int* arr2, int n2)
+{
+	if (n1 != n2)
+		return false;
+
+	else if (n1 == n2)
+	{
+		for (int i = 0; i < n1; i++)
+		{
+			if (arr1[i] != arr2[i])
+				return false;
+		}
+		return true;
+	}
+}*/
+
+/*Examples*/
+
+/* Enter a size of array: 5
+Array[0]: 1
+Array[1]: 2
+Array[2]: 3
+Array[3]: 5
+Array[4]: 4
+Enter a size of array: 5
+Array[0]: 2
+Array[1]: 4
+Array[2]: 1
+Array[3]: 5
+Array[4]: 3
+Array:
+1  2  3  5  4
+Array:
+2  4  1  5  3
+Res: 1 
+
+Enter a size of array: 3
+Array[0]: 3
+Array[1]: 2
+Array[2]: 1
+Enter a size of array: 3
+Array[0]: 1
+Array[1]: 5
+Array[2]: 3
+Array:
+3  2  1
+Array:
+1  5  3
+Res: 0
+
+Enter a size of array: 3
+Array[0]: 1
+Array[1]: 2
+Array[2]: 3
+Enter a size of array: 4
+Array[0]: 1
+Array[1]: 2
+Array[2]: 3
+Array[3]: 4
+Array:
+1  2  3
+Array:
+1  2  3  4
+Res: 0
+*/
 
 void read_arr(int*& arr, int& n)
 {
@@ -207,19 +299,19 @@ void print_arr(int* arr, int n)
 	}
 	cout << endl;
 }
-int n1, n2;
-bool check_anagram(int* arr1, int* arr2)
-{
-	if (n1 != n2)
-	{
-		return false;
-	}
 
-	for (int i = 0; i < n1; i++)
-	{
-		if (arr1[i] != arr2[i])
-			return false;
-		return true;
-	}
+void bubble_sort(int* arr, int n)
+{
+	int temp;
+	for (int j = 1; j < n; j++)
+		for (int i = 0; i < n - j; i++)
+			if (arr[i] > arr[i + 1])
+			{
+				temp = arr[i]; arr[i] = arr[i + 1]; arr[i + 1] = temp;
+			}
 }
+
+
+
+
 
